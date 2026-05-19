cc8b51acd174776eae75120c518b5600e84f9155
Bilel Mamoghli <bilelmamoghli06@gmail.com>
2026-05-19 17:59:31 -0400

    fonction getById() dans api.js

--- js/api.js
+++ js/api.js
@@ -1,3 +1,5 @@
+const BASE_URL = "http://localhost:8080/ords/commande/";
+
 async function getAll(endpoint) {
     try {
         const response = await fetch(`${BASE_URL}${endpoint}/`);
@@ -7,5 +9,16 @@ async function getAll(endpoint) {
     } catch (error) {
         console.error(error);
         return [];
+    }
+}
+
+async function getById(endpoint, id) {
+    try {
+        const response = await fetch(`${BASE_URL}${endpoint}/${id}`);
+        if (!response.ok) throw new Error(`Erreur HTTP: ${response.status}`);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return null;
     }
 }
